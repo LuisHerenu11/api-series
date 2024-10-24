c85110390b506d6abfd29d7b8930fd35194366e0
LuisHerenu11 <ElCondor_1106@hotmail.com>
2024-10-23 21:54:45 -0300

    actualizacion del index en carpeta src

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+// como no se especifica que archivo de rutas, tomara las que estén en el index de la carpeta rutas.
 const routes = require('./routes')
 const {genericMiddleware} = require('./middlewares')
 const db = require('./models')
@@ -10,10 +11,11 @@ app.use(express.json())
 app.use(genericMiddleware.requestTime)
 app.use(routes.seriesRoute)
 app.use(routes.usuariosRoute)
+app.use(routes.actoresRoute)
 app.listen(PORT, async ()=>{
     console.log(`Aplicación iniciada en el puerto ${PORT}`)
     // Esto lo hacemos solo en desarrollo para sincronizar el modelo con la db.
     // Descomentar solo cuando hay cambios en el modelo.
     // OJO: la sincronización forzada dropea todas las tablas (irreversible)
-    //db.sequelize.sync({force:true})
+    // db.sequelize.sync({force:true})
 })
